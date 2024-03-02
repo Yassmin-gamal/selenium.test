ccfdd9785bb2ef375a3feae976919d918ea3ccc8
Yassmin-gamal <66153260+Yassmin-gamal@users.noreply.github.com>
2024-03-02 04:27:02 +0200

    Update Test4.java

--- src/test/java/com/google/selenium/test/Test4.java
+++ src/test/java/com/google/selenium/test/Test4.java
@@ -32,7 +32,7 @@ public class Test4 {
 
 
 		JavascriptExecutor js = (JavascriptExecutor) driver;
-    js.executeScript("scroll(0, 200);");
+                js.executeScript("scroll(0, 200);");
 		TimeUnit.SECONDS.sleep(1);
 		js.executeScript("scroll(200, 600);");
 		TimeUnit.SECONDS.sleep(1);
